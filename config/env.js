6dddbf21361a4b9a968125ba886038d12bddb051
BrianHeredia <brianheredia@gmail.com>
2019-07-08 20:40:54 -0400

    username with host

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 const env = {
   database: 'heroku_35742a7059',
-  username: 'b8bb25885136e8',
+  username: 'b8bb25885136e8@us-cdbr-iron-east-02.cleardb.net',
   password: '5fa26cef',
   host: 'us-cdbr-iron-east-02.cleardb.net',
   dialect: 'mysql',
